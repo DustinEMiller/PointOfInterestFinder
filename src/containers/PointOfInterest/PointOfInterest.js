47e8ea8a142c87e79ccd55127b98ca0257e7d578
Dustin E Miller <dustinmiller032@gmail.com>
2019-11-05 14:40:20 -0600

    first commit

---
+++ src/containers/PointOfInterest/PointOfInterest.js
@@ -0,0 +1,16 @@
+import React, {Comnponent} from "react";
+
+class PointOfInterest extends Component {
+    render() {
+        return(
+            <Auxillary>
+                <div>Form</div>
+                <div>Map</div>
+                <div>List</div>
+            </Auxillary>
+        )
+    }
+
+}
+
+export default PointOfInterest;
